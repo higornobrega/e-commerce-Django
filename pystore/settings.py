95ec38ff43adf242aa421f39026def1ea4bf7347
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-08-31 13:51:44 -0300

    Configurando bd postgree

--- pystore/settings.py
+++ pystore/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'pystore.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
+
 
 
 # Password validation
