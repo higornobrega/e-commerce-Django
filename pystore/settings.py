0be5b3a4bc3fd41cf33c3ecb19818bb894d9ec72
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-08-31 14:19:05 -0300

    testes

--- pystore/settings.py
+++ pystore/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,4 +130,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# User Model
+AUTH_USER_MODEL = "users.User"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
